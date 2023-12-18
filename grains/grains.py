eb6a6cb153c27e5068c239dd76e08e90d9cfc544
李华林 <lihl10@cjsc.com.cn>
2023-12-18 09:58:30 +0800

    update

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,9 @@
-def square(number):
-    pass
+def square(number: int) -> int:
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+
+    return 1 << (number - 1)
 
 
 def total():
-    pass
+    return sum([square(x) for x in range(1, 65)])
